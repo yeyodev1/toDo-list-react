8bd1cae4a33bc246146d1bcee2342bdac20a82ab
Diego Reyes <diegorele13@gmail.com>
2023-05-16 19:20:22 -0500

    detecting: clicks and change inside from input

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,7 +1,17 @@
 import "./TodoSearch.css";
 
 function TodoSearch() {
-	return <input placeholder="Cortar cebolla" className="TodoSearch"></input>;
+	return (
+		<input
+			placeholder="Cortar cebolla"
+			className="TodoSearch"
+			onChange={(event) => {
+				console.log("ESCRIBISTE EN EL TODO SEARCH");
+				console.log(event);
+				console.log(event.target.value);
+			}}
+		></input>
+	);
 }
 
 export { TodoSearch };
