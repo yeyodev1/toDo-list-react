dcc0fe747b7589b8f667572660735effebaea8e4
Diego Reyes <diegorele13@gmail.com>
2023-05-25 11:43:46 -0500

    done: search todos

--- src/App.js
+++ src/App.js
@@ -31,6 +31,13 @@ function App() {
 	const completedTodos = todos.filter((todo) => !!todo.completed).length;
 	const totalTodos = todos.length;
 
+	const searchedTodos = todos.filter((todo) => {
+		const todoText = todo.text.toLocaleLowerCase();
+		const searchText = searchValue.toLowerCase();
+
+		return todoText.includes(searchText);
+	});
+
 	console.log(`los usuarios buscan todos de ${searchValue}`);
 
 	return (
@@ -42,7 +49,7 @@ function App() {
 			/>
 
 			<TodoList>
-				{defaultTodos.map((todo) => (
+				{searchedTodos.map((todo) => (
 					<TodoItem
 						key={todo.text}
 						text={todo.text}
